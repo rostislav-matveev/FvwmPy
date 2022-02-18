bc6aa61cd42d6a04e8cc8106ea236c434db9305e
rostislav-matveev <rostislav.matveev@googlemail.com>
2022-02-18 21:50:50 +0100

    str method for pickers

--- fvwmpy/picker.py
+++ fvwmpy/picker.py
@@ -2,45 +2,64 @@ import fnmatch
 
 ################################################################################
 ### some helpers
-from .log import _getloggers
-from .constants import *
-( logger, debug, info,
-  warn,   error, critical  ) = _getloggers('fvwmpy:picker')
-logger.setLevel(L_WARN)
+# from .log import _getloggers
+# from .constants import *
+# ( logger, debug, info,
+  # warn,   error, critical  ) = _getloggers('fvwmpy:picker')
+# logger.setLevel(L_WARN)
 ################################################################################
 
 class picker:
     def __init__(self, fcn=None, mask=None, **kwargs):
         if fcn is None:
             self.fcn = self._picker_factory(mask,**kwargs)
+            _str = list()
+            if mask:
+                _str.append("mask=0x{:x}".format(mask))
+            for k,v in kwargs.items():
+                _str.append("{}={}".format(k,v))
+            self._str="picker(" + ",".join(_str) + ")"
         elif mask is None and not kwargs:
             self.fcn = fcn
+            self._str= "picker({})".format(fcn)
         else:
             raise ValueError( "Wrong arguments signature. "+
                               "Must be either picker(fcn=fcn) or " +
                               "picker(mask=mask,**kwargs)" )
         
+    def __str__(self):
+        return self._str
+
+    __repr__ = __str__
+    
     def __call__(self,p):
         return self.fcn(p)
     
     def __and__(self,other):
         def fcn(p):
             return self.fcn(p) and other.fcn(p)
-        return picker(fcn=fcn)
+        pick = picker(fcn=fcn)
+        pick._str = "(" + self._str + " & " + other._str + ")"
+        return pick
 
     __rand__ = __and__
     
     def __or__(self,other):
         def fcn(p):
             return self.fcn(p) or other.fcn(p)
-        return picker(fcn=fcn)
+        pick = picker(fcn=fcn)
+        pick._str = "(" + self._str + " | " + other._str + ")"
+        return pick
 
     __ror__ = __or__
     
     def __invert__(self):
         def fcn(p):
             return not self.fcn(p)
-        return picker(fcn=fcn)
+        pick = picker(fcn=fcn)
+        pick._str = "~" + self._str
+        return pick
+
 
     @classmethod
     def _picker_factory(cls, mask=None,**kwargs):
@@ -48,11 +67,11 @@ class picker:
             debug("picker_fcn: working")
             if ( (mask is not None) and
                  (not p["ptype"] & mask) ):
-                debug("Check {}=p['ptype'] ?= {}",
-                      bin(p.get("ptype")),bin(mask))
+                # debug("Check {}=p['ptype'] ?= {}",
+                      # bin(p.get("ptype")),bin(mask))
                 return False
             for k,v in kwargs.items():
-                debug("Check {}=p[{}] ?= {}",p.get(k),k,v)
+                # debug("Check {}=p[{}] ?= {}",p.get(k),k,v)
                 if not ( k in p and p[k] == v ): return False
             return True
         return fcn
